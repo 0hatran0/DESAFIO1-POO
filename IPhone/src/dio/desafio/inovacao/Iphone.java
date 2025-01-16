403137e740638216bf38e12e11b3a89881df6220
0hatran0 <hatran.ms@aluno.ifsc.edu.br>
2025-01-15 21:17:29 -0300

    Metodos desenvolvidos e testados

--- IPhone/src/dio/desafio/inovacao/Iphone.java
+++ IPhone/src/dio/desafio/inovacao/Iphone.java
@@ -8,46 +8,46 @@ public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorI
 
     @Override
     public void ligar(String numero) {
-        
+        System.out.println("Ligando: " + numero);
     }
 
     @Override
     public void atender() {
-
+        System.out.println("Ligacao atendida");
     }
 
     @Override
     public void iniciarCorreioVoz() {
-
+        System.out.println("Iniciando Correio Voz");
     }
 
     @Override
     public void exibirPagina(String url) {
-
+        System.out.println("Pagina: " + url);
     }
 
     @Override
     public void adicionarNovaAba() {
-
+        System.out.println("Adicionando Nova Aba");
     }
 
     @Override
     public void atualizarPagina() {
-
+        System.out.println("Atualizando pagina");
     }
 
     @Override
     public void tocar() {
-
+        System.out.println("Tocando música");
     }
 
     @Override
     public void pausar() {
-
+        System.out.println("Música pausada");
     }
 
     @Override
     public void selecionarMusica(String musica) {
-
+        System.out.println("Selecionando musica: " + musica);
     }
 }
